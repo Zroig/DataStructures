4563eb91363fa303ae529bd90885accbe3980a2c
Mateo Cerna <mateocernasanchez@gmail.com>
2024-04-15 19:35:07 +0200

    EX1 Acabat

---
+++ src/EX1/EX1.java
@@ -0,0 +1,42 @@
+package EX1;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class EX1 {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> cartells = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        String cartell = "";
+        int metre = 0;
+        int c = 0;
+
+        c = sc.nextInt();
+
+        for (int i = 0; i < c; i++) {
+            metre = sc.nextInt();
+            cartell = sc.next();
+            cartells.put(metre, cartell);
+        }
+
+        while (true) {
+            
+            c = sc.nextInt();
+            
+            if (c == -1) {
+                 break; 
+            }
+
+            if (cartells.containsKey(c)) {
+                System.out.println(cartells.get(c));
+            } else {
+                System.out.println("No hi ha cartell");
+            }
+        }
+
+        sc.close();
+
+    }
+
+}
